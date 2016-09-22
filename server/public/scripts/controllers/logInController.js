a6612f2349733f023266975ea52d48f7ca1edb2a
Mat McCully <mccully1894@gmail.com>
2016-09-22 13:44:23 -0500

    base64 encoded added to login

--- server/public/scripts/controllers/logInController.js
+++ server/public/scripts/controllers/logInController.js
@@ -1,8 +1,13 @@
 eagleApp.controller('logInController' , ['$scope' , '$location' , 'Admin' , function($scope, $location, Admin){
-  $scope.admin = Admin;
+
 
   $scope.submitAdmin = function(){
     console.log($scope.admin);
+    var cred = window.btoa($scope.admin.username + ':' + $scope.admin.password);
+    $scope.admin.username = '';
+    $scope.admin.password = '';
+    Admin.setCred(cred);
+     console.log(cred);
     $location.path('/home')
   }
 }]);
