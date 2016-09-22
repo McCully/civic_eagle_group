9ee17a698514e5dcb79adc67bea18b858de55e14
Mat McCully <mccully1894@gmail.com>
2016-09-22 13:34:20 -0500

    admin service

--- server/public/scripts/services/admin.js
+++ server/public/scripts/services/admin.js
@@ -1,7 +1,17 @@
 eagleApp.factory('Admin' , ['$http' ,function($http) {
 
+  function setCred(cred) {
+
+  };
+
+  function getCred(){
+
+  };
+
 
 return{
-  
+  setCred: setCred,
+  getCred: getCred
+
 }
-}])
+}]);
