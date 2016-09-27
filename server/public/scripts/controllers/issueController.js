1d1731f215580c2e9f19e9aab967595d04cdaa4f
nah <2trill2spill@gmail.com>
2016-09-27 10:42:37 -0500

    Remove merge conflicts.

--- server/public/scripts/controllers/issueController.js
+++ server/public/scripts/controllers/issueController.js
@@ -1,10 +1,6 @@
 eagleApp.controller('issueController', ['$scope', '$http', 'Admin', 'textAngularManager', 'IssuesService', function ($scope, $http, Admin, textAngularManager, IssuesService) {
   var header = 'Basic ' + Admin.getCred();
   IssuesService.getIssues();
-<<<<<<< HEAD
-
-=======
->>>>>>> da8ea87d51aee3dd2c7bde76de488d950c0d70e8
   // $http({
   //   method: 'GET',
   //   url: "https://reps-staging.api.civiceagle.com/issues",
