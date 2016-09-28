245647158aafcd7fb17bd142c3e508ee0153f854
nah <2trill2spill@gmail.com>
2016-09-28 15:56:32 -0500

    Allow users to edit current candidate data.

--- server/public/scripts/controllers/logInController.js
+++ server/public/scripts/controllers/logInController.js
@@ -3,12 +3,10 @@
 eagleApp.controller('logInController' , ['$scope' , '$location' , 'Admin' , '$http', function($scope, $location, Admin, $http){
 
   $scope.submitAdmin = function(){
-    console.log($scope.admin);
     var cred = window.btoa($scope.admin.username + ':' + $scope.admin.password);
     $scope.admin.username = '';
     $scope.admin.password = '';
     Admin.setCred(cred);
-     console.log(cred);
 
      $http({
       method: 'POST',
