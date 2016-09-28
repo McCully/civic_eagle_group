beb0999752f831d1a39088291da6a6646b414c48
nah <2trill2spill@gmail.com>
2016-09-28 18:34:20 -0500

    Add comments explaining log in code.

--- server/public/scripts/controllers/logInController.js
+++ server/public/scripts/controllers/logInController.js
@@ -3,9 +3,16 @@
 eagleApp.controller('logInController' , ['$scope' , '$location' , 'Admin' , '$http', function($scope, $location, Admin, $http){
 
   $scope.submitAdmin = function(){
+    /* Generate the credentials from the username and password
+      so we can access the backends REST api. */
     var cred = window.btoa($scope.admin.username + ':' + $scope.admin.password);
+
+    /* Clear username and password. */
     $scope.admin.username = '';
     $scope.admin.password = '';
+
+    /* Store the credentials for later use by other
+       controllers and services.  */
     Admin.setCred(cred);
 
      $http({
