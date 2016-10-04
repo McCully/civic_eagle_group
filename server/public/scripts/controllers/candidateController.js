7c3b77835dd056304a78043b0e53402ce2ff1ab4
nah <2trill2spill@gmail.com>
2016-10-04 18:27:11 -0500

    Add comments and fix bug.
    Wrap the candidate and issue loading inside a else statment so we don't make bad requests to the server when the user is sent back to the login page.

--- server/public/scripts/controllers/candidateController.js
+++ server/public/scripts/controllers/candidateController.js
@@ -2,32 +2,21 @@
 
 eagleApp.controller('candidateController', ['$scope', '$location', '$http' ,'Admin', 'CandidateService', 'textAngularManager', 'issues',
 function($scope, $location, $http, Admin, CandidateService, textAngularManager, issues) {
-
   /* Create basic authorization header. */
   var header = 'Basic ' + Admin.getCred();
 
-  if(Admin.getCred() === undefined){
+ /* Make sure the user is authenticated. */
+ if(Admin.getCred() === undefined) {
    $location.path('/logIn');
+ } else {
+   /* Load all the candidate and issue information
+    so it can be displayed to the user. Load the resources
+    only if we are authenticated so that we don't
+    make a bunch of bad requests to the server
+    while the user is sent back to the login page. */
+   loadResources();
  };
-  /* Load all the candidates information so we can display it. */
-  CandidateService.getCandidates().then(function(response){
-    $scope.candidates = response;
-    console.log("Candidates: ", response);
-  }
-);
 
-$scope.issues = [];
-
-issues.getAll().then(function(response){
-  console.log(response);
-  for(var i = 0; i < response.length; i++) {
-    var id = response[i]._id;
-    issues.getById(id).then(function(response) {
-      console.log("Issue: ", response);
-      $scope.issues.push(response);
-    });
-  }
-});
 
 /* Grab the selected candidate's information and
  set it to a scope variable, so it can be displayed
@@ -57,5 +46,24 @@ $scope.candidateOrigHtml = '<h1>Candidates</h1>';
 $scope.candidateContent = $scope.candidateOrigHtml;
 $scope.disabled = false;
 
+function loadResources() {
+  /* Load all the candidates information so we can display it. */
+  CandidateService.getCandidates().then(function(response) {
+    $scope.candidates = response;
+    console.log("Candidates: ", response);
+  });
+
+  $scope.issues = [];
+
+  issues.getAll().then(function(response) {
+    for(var i = 0; i < response.length; i++) {
+      var id = response[i]._id;
+      issues.getById(id).then(function(response) {
+        console.log("Issue: ", response);
+        $scope.issues.push(response);
+      });
+    }
+  });
+}
 
 }]);
