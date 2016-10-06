b494ff0987b5d266a185ca72d16427c159d11823
nah <2trill2spill@gmail.com>
2016-10-06 10:55:16 -0500

    Add update and new candidate ajax calls.

--- server/public/scripts/services/candidateService.js
+++ server/public/scripts/services/candidateService.js
@@ -13,8 +13,37 @@ eagleApp.factory('Candidates' , ['$http', 'Admin', function($http, Admin) {
    return promise;
   }
 
+  function update(candidate) {
+    var id = candidate._id;
+    var promise = $http({
+     method: 'PUT',
+     data: candidate,
+     url: "https://reps-staging.api.civiceagle.com/candidates/" + id,
+     headers: {'Authorization': 'Basic ' + Admin.getCred()}
+   }).then(function(response) {
+     console.log("res: ", response);
+     return response.data;
+   });
+   return promise;
+  }
+
+  function add(candidate) {
+    var promise = $http({
+     method: 'POST',
+     data: candidate,
+     url: "https://reps-staging.api.civiceagle.com/candidates",
+     headers: {'Authorization': 'Basic ' + Admin.getCred()}
+   }).then(function(response) {
+     console.log("res: ", response);
+     return response.data;
+   });
+   return promise;
+  }
+
   return {
-    getAll: getAll
+    getAll: getAll,
+    update: update,
+    add: add
   };
 
 }]);
