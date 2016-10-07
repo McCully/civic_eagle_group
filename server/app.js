866eb84e4413c666315a91f537329ed9bfeff8c5
nah <2trill2spill@gmail.com>
2016-10-06 21:21:35 -0500

    Remove bodyparser and uneeded stuff from package.json

--- server/app.js
+++ server/app.js
@@ -2,11 +2,7 @@
 
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 var path = require('path');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, './public')));
 
